6d3ba0303ffe1b84efe728fbd773a8643a860655
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-12-20 15:20:07 -0400

    149 Generar un Json web token JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 const Usuario = require("../models/usuario");
 const bcryptjs = require("bcryptjs");
+const { generarJWT } = require("../helpers/generar-jwt");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
@@ -30,9 +31,11 @@ const login = async (req, res = response) => {
     }
 
     //Generar JWT
+    const token = await generarJWT(usuario.id);
 
     res.json({
-      msg: "Login OK",
+      usuario,
+      token,
     });
   } catch (error) {
     console.log(error);
