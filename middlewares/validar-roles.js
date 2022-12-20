3860cb0cb206923db8dce1b456c24b52449e56af
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-12-20 18:20:02 -0400

    154. Middleware: Tiene rol

--- middlewares/validar-roles.js
+++ middlewares/validar-roles.js
@@ -18,6 +18,26 @@ const esAdminRole = (req, res = response, next) => {
   next();
 };
 
+const tieneRole = (...roles) => {
+  return (req, res = response, next) => {
+    //console.log(roles, req.usuario.rol);
+    if (!req.usuario) {
+      return res.status(500).json({
+        msg: "Se quiere verificar el role sin validar el token primero",
+      });
+    }
+
+    if (!roles.includes(req.usuario.rol)) {
+      return res.status(401).json({
+        msg: `El servicio requiere uno de estos roles ${roles}`,
+      });
+    }
+
+    next();
+  };
+};
+
 module.exports = {
   esAdminRole,
+  tieneRole,
 };
