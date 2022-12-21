ab8f969a93495d8207a7c73a0bc6644795b9957f
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-12-21 18:40:41 -0400

    CRUD rutas y categorias

--- models/server.js
+++ models/server.js
@@ -6,8 +6,12 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
-    this.usuariosPath = "/api/usuarios";
-    this.authPath = "/api/auth";
+
+    this.paths = {
+      auth: "/api/auth",
+      categorias: "/api/categorias",
+      usuarios: "/api/usuarios",
+    };
 
     //Conectar a la base de datos
     this.conectarBD();
@@ -35,8 +39,9 @@ class Server {
   }
 
   routes() {
-    this.app.use(this.authPath, require("../routes/auth"));
-    this.app.use(this.usuariosPath, require("../routes/usuario"));
+    this.app.use(this.paths.auth, require("../routes/auth"));
+    this.app.use(this.paths.categorias, require("../routes/categorias"));
+    this.app.use(this.paths.usuarios, require("../routes/usuario"));
   }
 
   listen() {
